5d4e8aa1ab3c860e1c4c1853772aa486fef130ac
Roman Zubko <skcorpoma@gmail.com>
2019-11-25 17:38:51 +0200

    Updates

--- js/main.js
+++ js/main.js
@@ -31,3 +31,14 @@ function toggleMenu() {
     showMenu = false;
   }
 }
+
+$(window).scroll(function() {
+  var scrollTop = $(this).scrollTop();
+
+  $(".header-overlay").css({
+    opacity: function() {
+      var elementHeight = $(this).height();
+      return 1 - (elementHeight - scrollTop) / elementHeight;
+    }
+  });
+});
